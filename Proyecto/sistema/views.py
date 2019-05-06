8082c2ce3fc70960fd98e2566906425ebc9b043c
Felipe Canales <felipe.canales.213@gmail.com>
2019-05-06 18:11:56 -0400

    Creadas las urls para las distintas páginas.

--- Proyecto/sistema/views.py
+++ Proyecto/sistema/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index_cursos(request):
+    return
+
+def index_evaluaciones(request):
+    return
+
+def index_evaluadores(request):
+    return
+
+def index_rubricas(request):
+    return
+
+def evaluacion(request, eval_id):
+    if True:
+        return
+    return
+
+def postevaluacion(request, eval_id):
+    return
+
+def rubrica(request, rubrica_id):
+    return
