66f0bf6669418305dc21b060b9fa0d917c308f2b
Felipe Canales <felipe.canales.213@gmail.com>
2019-05-06 20:10:29 -0400

    Ahora se cargan las páginas

--- Proyecto/sistema/views.py
+++ Proyecto/sistema/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
+from .models import Instancia, Evaluador, Evaluacion, Rubrica
 
 def index_cursos(request):
-    return
+    context = {
+        'lista_cursos': Instancia.objects.all()
+    }
+    return render(request, 'sistema/admin/pag_cursos.html', context)
 
 def index_evaluaciones(request):
-    return
+    context = {
+        'lista_evaluaciones': Evaluacion.objects.all()
+    }
+    return render(request, 'sistema/admin/pag_evaluaciones.html', context)
 
 def index_evaluadores(request):
-    return
+    context = {
+        'lista_evaluadores': Evaluador.objects.all()
+    }
+    return render(request, 'sistema/admin/pag_evaluadores.html', context)
 
 def index_rubricas(request):
-    return
+    context = {
+        'lista_rubricas': Rubrica.objects.all()
+    }
+    return render(request, 'sistema/admin/pag_rubricas.html', context)
 
 def evaluacion(request, eval_id):
     if True:
