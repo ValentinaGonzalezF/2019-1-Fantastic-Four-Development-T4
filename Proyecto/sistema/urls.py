16977f0342286d59168df13bca6f166ab2e324c6
felipe-canales <felipe.canales.213@gmail.com>
2019-05-09 16:03:22 -0400

    Reordenadas las urls

--- Proyecto/sistema/urls.py
+++ Proyecto/sistema/urls.py
@@ -21,33 +21,40 @@ from . import views
 app_name = 'sistema'
 
 urlpatterns = [
-    
+	# LANDING
+	path('landingpage/', views.index_landing, name='index_landing'),
+
+    # LOGIN
     path('login/', views.index_login, name = 'index_login'),
-    
-    
+	
+    # INDICES
     path('cursos/', views.index_cursos, name = 'index_cursos'),
     path('evaluaciones/', views.index_evaluaciones, name = 'index_evaluaciones'),
     path('evaluadores/', views.index_evaluadores, name = 'index_evaluadores'),
     path('rubricas/', views.index_rubricas, name = 'index_rubricas'),
+	
+	# EVALUACION
     path('evaluaciones/<int:eval_id>/', views.evaluacion, name = 'evaluacion'),
+	path('grupos/', views.gruposevaluacion, name='gruposevaluacion'),
+    path('evaluacion/', views.evaluacion, name='evaluacion'),
+    path('evaluacionfinalizada/', views.postevaluacion , name='postevaluacion'),
+	
+	# RUBRICA
     path('rubricas/<int:rubrica_id>/', views.rubrica, name = 'rubrica'),
     path('rubricas/<int:rubrica_id>/editar', views.rubrica_editar, name = 'rubrica_editar'),
-
+	
+	# GESTIONAR EVALUADORES
     path('evaluadores/agregar/', views.agregar_evaluador, name = 'evaluador_agr'),
     path('evaluadores/modificar/', views.modificar_evaluador, name = 'evaluador_mod'),
     path('evaluadores/eliminar/', views.eliminar_evaluador, name = 'evaluador_eli'),
-
+	
+	# GESTIONAR RUBRICAS
     path('rubricas/agregar/', views.agregar_rubrica, name = 'rubrica_agr'),
     path('rubricas/<int:rubrica_id>/guardar', views.modificar_rubrica, name = 'rubrica_mod'),
     path('rubricas/eliminar/', views.eliminar_rubrica, name = 'rubrica_eli'),
-
+	
+	# GESTIONAR EVALUACIONES
     path('evaluaciones/agregar/', views.agregar_evaluacion, name = 'evaluacion_agr'),
     path('evaluaciones/modificar/', views.modificar_evaluacion, name = 'evaluacion_mod'),
     path('evaluaciones/eliminar/', views.eliminar_evaluacion, name = 'evaluacion_eli'),
-
-    path('grupos/', views.gruposevaluacion, name='gruposevaluacion'),
-    path('evaluacion/', views.evaluacion, name='evaluacion'),
-    path('evaluacionfinalizada/', views.postevaluacion , name='postevaluacion'),
-
-    path('landingpage/', views.index_landing, name='index_landing'),
 ]
